f56d164de1170b038dd069f070123989c425219a
uehara-hazuki <hotaru103bsb@gmail.com>
2020-05-27 21:37:24 +0900

    add dropdown features

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,9 @@
+console.log("こんにちは");
 $(document).ready(function () {
     $('.collapsible').collapsible();
+});
+$(function () {
+    $(".dropdown-trigger").dropdown({
+        coverTrigger: false
+    });
 });
