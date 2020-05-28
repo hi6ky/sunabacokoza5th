557195530c9d19ae6a8c678f71f9e7a9185b0134
Yuka Onaga <gurigura00@gmail.com>
2020-05-28 20:46:51 +0900

    掲示板投稿ページを直しました

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def regist_post():
     ut = datetime.datetime.now()
     conn = sqlite3.connect("lgbt.db")
     c = conn.cursor()
-    c.execute("insert into user values(null, ?, ?, ?, ?, ?, ?)", (first,last,idname,password,email,ut))
+    c.execute("insert into user values(null, ?, ?, ?, ?, ?, ?, 0)", (first,last,idname,password,email,ut,))
     conn.commit()
     c.close()
     return render_template("top_login.html")
